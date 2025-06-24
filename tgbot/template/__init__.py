fb504d28aa930b715edceb9cffe5afc366dca8a6
Ivan Petylo <petylo.work@gmail.com>
2025-06-25 00:21:49 +0300

    Fixed tgbot.template being not a module

---
+++ tgbot/template/__init__.py
@@ -0,0 +1,9 @@
+"""
+Template package for the bot.
+
+This package contains all the text templates used in the bot.
+"""
+
+from tgbot.template.user_messages import *
+from tgbot.template.button_labels import *
+from tgbot.template.question_templates import *
